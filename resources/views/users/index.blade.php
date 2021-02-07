5698ace913a656e0da25c9ab5e02f1d4e2445528
Jonathan <mcjhona069@gmail.com>
2021-02-06 20:31:51 -0600

    Creando formulario

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,7 +16,34 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8 mx-auto">
-                
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                         @if($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach($errores->all() as $error)
+                                -{{ $error }} <br>
+                                @endforeach
+                            </div>
+                         @endif
+                        <form action="{{ route('users.store') }}" method="POST">
+                            <div class="form-row">
+                                <div class="col-sm-3">
+                                    <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name) }}">
+                                </div>
+                                <div class="col-sm-4">
+                                    <input type="text" name="email" class="form-control" placeholder="email" value="{{ old('email) }}">
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="text" name="password" class="form-control" placeholder="Contraseña">
+                                </div>
+                                <div class="col-auto">
+                                    @csrf
+                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                </div>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
 
                     <table class="table">
                         <thead>
